112ebea626f75c3850afdc80d900a4f61f835c2d
Gab Pedroso <gbpedroso.22@gmail.com>
2023-06-11 14:56:02 -0300

    js ajustado

--- src/script.js
+++ src/script.js
@@ -1,9 +1,27 @@
-function show() {
-    this.document.getElementById("card").style.removeProperty('display')
+function showSobreMim() {
+    document.getElementById("card-sobre-mim").style.removeProperty('display')
+    document.querySelector(".container-front").classList.remove('index-front')
+}
+
+function showSkills() {
+    document.getElementById("card-skills").style.removeProperty('display')
+    document.querySelector(".container-front").classList.remove('index-front')
+}
+
+function showCertificados() {
+    document.getElementById("card-certificados").style.removeProperty('display')
+    document.querySelector(".container-front").classList.remove('index-front')
+}
+
+function showProjetos() {
+    document.getElementById("card-projetos").style.removeProperty('display')
     document.querySelector(".container-front").classList.remove('index-front')
 }
 
 function exitCards() {
-    this.document.getElementById("card").style.display = "none"
+    document.getElementById("card-sobre-mim").style.display = "none"
+    document.getElementById("card-skills").style.display = "none"
+    document.getElementById("card-certificados").style.display = "none"
+    document.getElementById("card-projetos").style.display = "none"
     document.querySelector(".container-front").classList.add('index-front')
 }
